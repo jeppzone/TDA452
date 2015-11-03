9dcfe738b7c95a1d5757903323744012b112ab8b
Jesper Olsson <jeppealle@hotmail.com>
2015-11-03 12:28:09 +0100

    Added labfile

---
+++ Lab1/Lab1.hs
@@ -0,0 +1,11 @@
+--	Part 1:
+	-- It will take x computing steps, since
+
+--	Part 2:
+
+power1 :: Integer -> Integer -> Integer
+power1 n k = product([ n | x <- [1..k] ])
+ 
+--	Part 3:
+
+--	Part 4:
