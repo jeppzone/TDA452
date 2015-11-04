d8d99470b00c6e9c7ef268040551681bd0f35038
Mightoz <oscar.beronius@gmail.com>
2015-11-04 13:28:18 +0100

    Added answer for question 3.

--- Lab1/Lab1.hs
+++ Lab1/Lab1.hs
@@ -1,11 +1,32 @@
---	Part 1:
-	-- It will take x computing steps, since
+module Lab1 where
+import System.Environment
 
---	Part 2:
+----------------Part 1----------------
+
+power :: Integer -> Integer -> Integer
+power n k | k < 0 = error "power: negative argument"
+power n 0 = 1
+power n k = n * power n (k-1)
+
+--How many computing steps are being used?
+--Answer: k steps + base case -> (k+1) steps.
+
+----------------Part 2----------------
 
 power1 :: Integer -> Integer -> Integer
 power1 n k = product([ n | x <- [1..k] ])
- 
---	Part 3:
 
---	Part 4:
+--Version 2:
+--power1 :: Integer -> Integer -> Integer
+--power1 n k = product(replicate(fromInteger k) n)
+ 
+----------------Part 3----------------
+
+power2 :: Integer -> Integer -> Integer
+power2 n 0 = 1
+power2 n k | k < 0 = error "power: negative argument"
+power2 n k | even k = power2 (n*n) (div k 2) 
+power2 n k | odd k = n * power2 n (k-1)
+
+
+----------------Part 4----------------
