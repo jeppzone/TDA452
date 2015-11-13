286f5829db225ec70c0b292f44180922539f9f78
Jesper Olsson <jeppealle@hotmail.com>
2015-11-13 17:58:00 +0100

    Solved task 1, 4 and 5 of the rejection

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -2,12 +2,13 @@ module BlackJack where
 import Cards
 import Wrapper
 
+
 -----------Task 2-----------
 {-
 	size hand2 = size (Add (Card (Numeric 2) Hearts)
 					        (Add (Card Jack Spades) Empty))
 	= 1 + size (Add (Card Jack Spades) Empty)
-	= 1 + 1 + size(Empty)
+	= 1 + (1 + size(Empty))
 	= 1 + 1 + 0
 	= 2
 -}
@@ -17,55 +18,45 @@ import Wrapper
 -- Returns an empty hand
 
 empty :: Hand
-empty = Empty;
+empty = Empty
 
 -- Returns the value of the rank
 
 valueRank :: Rank -> Integer
-valueRank (Numeric number) 			= number
-valueRank rank 	| rank == Jack 	= 10
-								| rank == Queen = 10
-								| rank == King 	= 10
-								| rank == Ace 	= 11
+valueRank (Numeric n) 		   = n
+valueRank r | r == Ace 		   = 11
+			      | otherwise 	   = 10
 
 -- Returns the value of the card
 
 valueCard :: Card -> Integer
-valueCard (Card rank suit) = valueRank rank
+valueCard c = valueRank (rank c)
 
 -- Returns the number of aces in a hand
 
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
-numberOfAces (Add (Card r s) hand ) | r == Ace  = 1 + numberOfAces hand
-																		| otherwise =  numberOfAces hand
+numberOfAces (Add (Card r s) hand) | r == Ace  = 1 + numberOfAces hand
+								   | otherwise = numberOfAces hand
 
--- Helper functions for calculating the highest 
--- and lowest possible values of a hand
-
-valueHigh :: Hand -> Integer
-valueHigh Empty = 0
-valueHigh (Add card hand) = valueCard card + valueHigh hand
-
-valueLow :: Hand -> Integer
-valueLow hand =  (valueHigh hand) - (numberOfAces hand) * 10 
-
--- Returns the real blackjack value of a hand
+-- Returns the value of a hand using blackjack rules.
 
 value :: Hand -> Integer
-value Empty = 0
-value (Add card hand) | valueHigh (Add card hand) > 21 = valueLow(Add card hand)
-					  					| otherwise = valueHigh(Add card hand)
+value (Add c h) | value2 (Add c h) <= 21 = valueCard c + value h
+				| otherwise = value2 (Add c h) - (numberOfAces h)*10
+				where
+					value2 Empty = 0
+					value2 (Add c h) = valueCard c + value2 h
 
 -- Returns true if the game is over, false otherwise
 
 gameOver :: Hand -> Bool
-gameOver hand = (value hand) > 21
+gameOver hand = value hand > 21
 
 -- Returns the winner of the round
 
 winner :: Hand -> Hand -> Player
-winner guest bank | gameOver guest = Bank
-				  				| gameOver bank  = Guest
-				  				| value bank >= value guest = Bank
-				  				| otherwise = Guest
+winner ph bh | (gameOver ph) = Bank
+			 | (gameOver bh) = Guest
+			 | value ph > value bh = Guest
+ 			 | value ph <= value bh = Bank
