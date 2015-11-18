d11008182d145840457fbeb206c068b6276dd469
Jesper Olsson <jeppealle@hotmail.com>
2015-11-18 22:09:36 +0100

    indentatinon

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -8,11 +8,11 @@ import Test.QuickCheck
 -----------Task 2-----------
 {-
 	size hand2 = size (Add (Card (Numeric 2) Hearts)
-					        (Add (Card Jack Spades) Empty))
-	= 1 + size (Add (Card Jack Spades) Empty)
-	= 1 + (1 + size(Empty))
-	= 1 + 1 + 0
-	= 2
+	                  (Add (Card Jack Spades) Empty))
+  = 1 + size (Add (Card Jack Spades) Empty)
+  = 1 + (1 + size(Empty))
+  = 1 + 1 + 0
+  = 2
 -}
 
 -----------Task 3-----------
