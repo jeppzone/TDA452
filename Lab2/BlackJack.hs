245989e888d3256e668bf0f8fa7c988abfa5cfb7
Jesper Olsson <jeppealle@hotmail.com>
2015-11-18 08:07:09 +0100

    Lab 2 done

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -1,6 +1,8 @@
 module BlackJack where
 import Cards
 import Wrapper
+import System.Random
+import Test.QuickCheck
 
 
 -----------Task 2-----------
@@ -36,16 +38,16 @@ valueCard c = valueRank (rank c)
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add (Card Ace s) hand) = 1 + numberOfAces hand
-numberOfAces (Add _ hand)            = numberOfAces hand
+numberOfAces (Add _ hand) = numberOfAces hand
 
 -- Returns the value of a hand using blackjack rules.
 
 value :: Hand -> Integer
-value (Add c h) | value2 (Add c h) <= 21 = valueCard c + value h
-                | otherwise              = value2 (Add c h) - (numberOfAces h)*10
-                  where
-                       value2 Empty      = 0
-                       value2 (Add c h)  = valueCard c + value2 h
+value h | value2 h <= 21 = value2 h
+        | otherwise = value2 h - (numberOfAces h)*10
+  where
+    value2 Empty      = 0
+    value2 (Add c h)  = valueCard c + value2 h
 
 -- Returns true if the game is over, false otherwise
 
@@ -59,3 +61,106 @@ winner ph bh | (gameOver ph) = Bank
              | (gameOver bh) = Guest
              | value ph > value bh = Guest
              | value ph <= value bh = Bank
+
+-- Operator to add a hand on top of another hand
+
+(<+) :: Hand -> Hand -> Hand
+Empty <+ hand2 = hand2
+(Add card hand) <+ hand2 = (Add card (hand <+ hand2))
+
+-- Property to check if the <+ operator is associative
+
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc h1 h2 h3 = h1 <+(h2 <+ h3) == (h1 <+ h2) <+ h3
+
+-- Property to check if the size is preserved after using the <+ operator
+
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf h1 h2 = (size h1) + (size h2) == size(h1 <+ h2)
+
+-- Returns a hand consisting of all cards of the given suit
+
+fullSuit :: Suit -> Hand
+fullSuit s = (Add (Card Ace s) (Add (Card King s) (Add (Card Queen s) 
+	           (Add (Card Jack s)(Add (Card (Numeric 10) s) (Add (Card (Numeric 9) s) 
+	           (Add (Card (Numeric 8 ) s)(Add (Card (Numeric 7) s) 
+	           (Add (Card (Numeric 6 ) s) (Add (Card (Numeric 5) s) 
+             (Add (Card (Numeric 4 ) s) (Add (Card (Numeric 3) s) 
+             (Add (Card (Numeric 2 ) s) Empty )))))))))))))
+
+-- Returns a full deck consisting of all 52 cards
+
+fullDeck :: Hand
+fullDeck = (fullSuit Hearts) <+ (fullSuit Diamonds) <+ 
+           (fullSuit Clubs) <+ (fullSuit Spades)
+
+-- Draws a card from the deck, returns error if deck is empty
+
+draw :: Hand -> Hand -> (Hand, Hand)
+draw Empty _ = error ("draw: The deck is empty")
+draw (Add card deck) hand = (deck, (Add card hand))
+
+-- Plays a bank hand
+playBank :: Hand -> Hand
+playBank deck = playBank' deck Empty
+
+-- Helper function for playing a bank hand
+playBank' :: Hand -> Hand -> Hand
+playBank' deck hand | value hand >= 16 = hand
+                    | otherwise        = uncurry playBank' play
+  where play = draw deck hand
+
+-- Shuffles a hand of cards using a random number generator
+
+shuffle :: StdGen -> Hand -> Hand
+shuffle _ Empty = Empty
+shuffle g deck  = Add removed (shuffle g' partial)
+  where
+    deckSize           = size deck
+    (n, g')            = randomR (1, deckSize) g
+    (partial, removed) = removeCard deck n Empty
+
+-- Removes a card from a hand at given position n and moves it to the other hand.
+-- Returns a tuple of the changed changed hand and the removed card
+
+removeCard :: Hand -> Integer -> Hand -> (Hand, Card)
+removeCard (Add card h1) n h2
+   | n == 1      = (h1 <+ h2, card)
+   | otherwise   = removeCard h1 (n-1) (Add card h2)
+
+-- Checks if a given card belongs to hand
+
+belongsTo :: Card -> Hand -> Bool
+c `belongsTo` Empty = False
+c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
+
+-- Property to check if a shuffled hand contains the same cards
+-- as it dit before
+
+prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
+prop_shuffle_sameCards g c h = c `belongsTo` h == c `belongsTo` shuffle g h
+
+-- Property to check wether the size of a shuffled hand is preserved
+
+prop_size_shuffle :: StdGen -> Hand -> Bool
+prop_size_shuffle g h = (size h) == size (shuffle g h)
+
+-- Tells the program which functions implements the defined interfaces
+
+implementation = Interface{ 
+  iEmpty = empty
+, iFullDeck = fullDeck
+, iValue = value
+, iGameOver = gameOver
+, iWinner = winner
+, iDraw = draw
+, iPlayBank = playBank
+, iShuffle = shuffle
+}
+
+-- Main function that runs the program
+
+main :: IO()
+main = runGame implementation
+
+
