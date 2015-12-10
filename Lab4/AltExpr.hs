0546ae4227e1df5b382697d046a5f12dd41c9b09
Jesper Olsson <jeppealle@hotmail.com>
2015-12-10 22:14:38 +0100

    Fixed some stuff

--- Lab4/AltExpr.hs
+++ Lab4/AltExpr.hs
@@ -3,10 +3,10 @@ module Expr where
 import Parsing
 
 data Expr = Num Double
-          | BOP Op Expr Expr
-          | UOP Fun Expr
+          | Bop Op Expr Expr
+          | Uop Fun Expr
           | Var String
-        deriving Eq
+  deriving Eq
 
 data Op = Add | Sub | Mul | Div 
   deriving Eq
@@ -18,25 +18,32 @@ data Fun = Sin | Cos
 instance Show Expr where
     show = showExpr
 
-
 showExpr :: Expr -> String
-showExpr (Num n)     = show n
-showExpr (BOP o e1 e2) = showExpr e1 ++ showOp o ++ showExpr e2 
+showExpr (Num n)       = show n
+showExpr (Bop o e1 e2) = showExpr e1 ++ showOp o ++ showExpr e2
+showExpr (Uop f e)     = showFun f ++ showExpr e
+showExpr (Var x)       = x
 --showExpr (UOP f e) = showFun ++ showExpr e
 
 showOp :: Op -> String
-showOp o | o == Add = " + "
-         | o == Sub = " - "
-         | o == Mul = " * "
-         | o == Div = " / "
+showOp Add = " + "
+showOp Sub = " - "
+showOp Mul = " * "
+showOp Div = " / "
 
+showFun :: Fun -> String
+showFun Sin = "sin "
+showFun Cos = "cos "
 
 eval :: Expr -> Double -> Double
 eval (Num n) _ = n
-eval (BOP Add e1 e2) x = (eval e1 x) + (eval e2 x)
-eval (BOP Sub e1 e2) x = (eval e1 x) - (eval e2 x)
-eval (BOP Mul e1 e2) x = (eval e1 x) * (eval e2 x)
-eval (BOP Div e1 e2) x = (eval e1 x) / (eval e2 x)
-eval (UOP Sin e1) x    = sin (eval e1 x)
-eval (UOP Cos e1) x    = cos (eval e1 x)
+eval (Bop Add e1 e2) x = (eval e1 x) + (eval e2 x)
+eval (Bop Sub e1 e2) x = (eval e1 x) - (eval e2 x)
+eval (Bop Mul e1 e2) x = (eval e1 x) * (eval e2 x)
+eval (Bop Div e1 e2) x = (eval e1 x) / (eval e2 x)
+eval (Uop Sin e1) x    = sin (eval e1 x)
+eval (Uop Cos e1) x    = cos (eval e1 x)
 eval (Var s) x         = x
+
+readExpr :: String -> Expr
+readExpr = undefined
