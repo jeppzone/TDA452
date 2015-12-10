c85a6140c7a16b77b1aae7db97cdf6df939fe5fb
Mightoz <oscar.beronius@gmail.com>
2015-12-10 22:33:39 +0100

    Made code nicer.

--- Lab4/Expr.hs
+++ Lab4/Expr.hs
@@ -1,33 +1,47 @@
 module Expr where
 
+import Parsing
+
 data Expr = Num Double
-        | Mul Expr Expr
-        | Add Expr Expr
-        | Sin Expr
-        | Cos Expr
+        | Bop Op Expr Expr
+        | Uop Fun Expr
         | Var String
         deriving Eq
-
--- type X = String
 
 instance Show Expr where
     show = showExpr
 
+data Op = Mul | Add
+    deriving Eq
+
+data Fun = Sin | Cos
+    deriving Eq
+
+
+showOp :: Op -> String
+showOp  o  | o == Mul = "*"
+           | o == Add = "+"
+
+showFun :: Fun -> String
+showFun  f | f == Sin = "Sin("
+           | f == Cos = "Cos("
+
 
 showExpr :: Expr -> String
-showExpr (Num n)        = show n
-showExpr (Mul e1 e2)    = show e1 ++ " * " ++ show e2
-showExpr (Add e1 e2)    = show e1 ++ " + " ++ show e2
-showExpr (Sin e)        = "Sin " ++ "(" ++ show e ++ ")"
-showExpr (Cos e)        = "Cos " ++ "(" ++ show e ++ ")"
-showExpr (Var e)        = "x"
+showExpr (Num n)            = show n
+showExpr (Bop o e1 e2)      = show e1 ++ showOp o ++ show e2
+showExpr (Uop f e)          = showFun f ++ show e ++ ")"
+showExpr (Var e)            = "x"
 
 
 
 eval :: Expr -> Double -> Double
-eval (Num n)       d    = n
-eval (Mul e1 e2)   d    = eval e1 d * eval e2 d
-eval (Add e1 e2)   d    = eval e1 d + eval e2 d
-eval (Sin e)       d    = sin (eval e d)
-eval (Cos e)       d    = cos (eval e d)
-eval (Var e)       d    = d
+eval (Num n)             _    = n
+eval (Bop Mul e1 e2)     d    = eval e1 d * eval e2 d
+eval (Bop Add e1 e2)     d    = eval e1 d + eval e2 d
+eval (Uop Sin e)         d    = sin (eval e d)
+eval (Uop Cos e)         d    = cos (eval e d)
+eval (Var e)             d    = d
+
+readExpr :: String -> Maybe Expr
+readExpr = undefined
