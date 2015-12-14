1153b03311d96917957a3ff1c93e317ad1621e41
Jesper Olsson <jeppealle@hotmail.com>
2015-12-14 21:34:57 +0100

    Fixed some simplifying

--- Lab4/AltExpr.hs
+++ Lab4/AltExpr.hs
@@ -12,7 +12,7 @@ data Expr = Num Double
           | Var
   deriving Eq
 
-data Op = Add | Sub | Mul | Div 
+data Op = Add | Mul 
   deriving Eq
 
 data Fun = Sin | Cos
@@ -20,54 +20,114 @@ data Fun = Sin | Cos
 
 
 instance Show Expr where
-    show = showExpr
+  show = showExpr
 
+--Converts an Expression into a String
 showExpr :: Expr -> String
-showExpr (Num n)       = show n
-showExpr (Bop o e1 e2) = showExpr e1 ++ showOp o ++ showExpr e2
-showExpr (Uop f e)     = showFun f ++ showExpr e
-showExpr (Var)         = "x"
---showExpr (UOP f e) = showFun ++ showExpr e
+showExpr (Num n) = show n
+showExpr (Bop Add a b) = showExpr a ++ "+" ++ showExpr b
+showExpr (Bop Mul a b) = showFactor a ++ "*" ++ showFactor b
+showExpr (Var) = "x"
+showExpr (Uop Sin z) = "sin " ++ showRads z
+showExpr (Uop Cos z) = "cos " ++ showRads z
 
-showOp :: Op -> String
-showOp Add = " + "
-showOp Sub = " - "
-showOp Mul = " * "
-showOp Div = " / "
+showFactor :: Expr -> String
+showFactor (Bop Add a b) = "(" ++ showExpr (Bop Add a b) ++ ")"
+showFactor e = showExpr e
 
-showFun :: Fun -> String
-showFun Sin = "sin "
-showFun Cos = "cos "
+showRads :: Expr -> String
+showRads (Bop Add a b) = "(" ++ showExpr (Bop Add a b) ++ ")"
+showRads (Bop Mul a b) = "(" ++ showExpr (Bop Mul a b) ++ ")"
+showRads e = showExpr e
 
 eval :: Expr -> Double -> Double
 eval (Num n) _ = n
 eval (Bop Add e1 e2) x = (eval e1 x) + (eval e2 x)
-eval (Bop Sub e1 e2) x = (eval e1 x) - (eval e2 x)
 eval (Bop Mul e1 e2) x = (eval e1 x) * (eval e2 x)
-eval (Bop Div e1 e2) x = (eval e1 x) / (eval e2 x)
 eval (Uop Sin e1) x    = sin (eval e1 x)
 eval (Uop Cos e1) x    = cos (eval e1 x)
 eval (Var) x           = x
 
 readExpr :: String -> Maybe Expr
-readExpr = undefined
+readExpr s = do
+         e <- parse parseAdd (removeSpaces s)
+         return $ fst e
+
+parseAdd :: Parser Expr
+parseAdd = foldr1(Bop Add) `fmap` chain parseMul (parseOperator '+') 
+
+parseMul :: Parser Expr
+parseMul = foldr1 (Bop Mul) `fmap` chain parseFactor (parseOperator '*')
+
+parseOperator :: Char -> Parser String
+parseOperator c = do
+              string[c]
+
+parseFactor :: Parser Expr
+parseFactor = parseParenthesis +++
+         parseFunction Sin "sin" +++
+         parseFunction Cos "cos" +++
+         parseVariable +++
+         parseNumber
+
+parseParenthesis :: Parser Expr
+parseParenthesis = do
+                   char '('
+                   e <- parseAdd
+                   char ')'
+                   return $ e
+parseFunction :: Fun -> String -> Parser Expr
+parseFunction f s = do
+                    string s
+                    e <- parseFactor
+                    return $ Uop f e
+
+parseVariable :: Parser Expr
+parseVariable = do
+                char 'x'
+                return Var
+
+parseNumber :: Parser Expr
+parseNumber = do
+              d <- readsP :: Parser Double
+              return (Num d)
+
+string :: String -> Parser String
+string s = sequence $ fmap (char) s
+
+removeSpaces :: String -> String
+removeSpaces [] = ""
+removeSpaces (' ':xs) = removeSpaces xs
+removeSpaces (x:[]) = [x]
+removeSpaces (x:xs) = x : removeSpaces xs
 
 simplify :: Expr -> Expr
-simplify (Bop Add (Num n1) (Num n2)) = Num (n1 + n2)
-simplify exp@(Bop Add (Num n) e)
+simplify (Bop o e1 e2) = simplify' (Bop o (simplify e1) (simplify e2))
+simplify (Uop f e) = simplify' (Uop f (simplify e))
+simplify e = e
+
+simplify' :: Expr -> Expr
+simplify' (Bop Add (Num n1) (Num n2)) = Num (n1 + n2)
+simplify' exp@(Bop Add (Num n) e)
        | n == 0    = simplify e
        | otherwise = exp
-simplify exp@(Bop Add e (Num n))
+simplify' exp@(Bop Add e (Num n))
        | n == 0    = simplify e
        | otherwise = exp
-simplify (Bop Mul (Num n1) (Num n2)) = Num (n1*n2)
-simplify exp@(Bop Mul (Num n) e)
+simplify' exp@(Bop Add (Var) e) = (Bop Add (Num 0) (simplify e))
+simplify' exp@(Bop Add e (Var)) = (Bop Add (Num 0) (simplify e))
+simplify' (Bop Mul (Num n1) (Num n2)) = Num (n1*n2)
+simplify' exp@(Bop Mul (Num n) e)
        | n == 0    = Num 0
        | n == 1    = simplify e
        | otherwise = exp
-simplify exp@(Bop Mul e (Num n))
+simplify' exp@(Bop Mul e (Num n))
        | n == 0    = Num 0
-simplify e = e
+       | n == 1    = simplify e
+       | otherwise = exp
+simplify' exp@(Bop Mul (Var) e) = (Bop Mul (Num 1) (simplify e))
+simplify' exp@(Bop Mul e (Var)) = (Bop Mul (Num 1) (simplify e))
+simplify' e = e
 
 
 differentiate :: Expr -> Expr
--- Lab4/ExprQC.hs
+++ Lab4/ExprQC.hs
@@ -2,13 +2,26 @@ import Test.QuickCheck
 import Data.Maybe
 import AltExpr
 
-prop_ŚhowReadExpr :: Expr -> Bool
-prop_ŚhowReadExpr e = closeToEqual e (fromJust(readExpr (showExpr e)))
-  where closeToEqual e1 e2 = abs ((eval' e1) - (eval' e2)) < 0.01
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr e = closeToEqual e (fromJust(readExpr (showExpr e)))
+  where closeToEqual e1 e2 = abs ((eval' e1) - (eval' e2)) < 0.0001
         eval' e = eval e 1.37
 
 arbExpr :: Int -> Gen Expr
-arbExpr n = undefined
+arbExpr n | n == 1 = oneOf [number, variable]
+  where 
+    number = do
+                nbr <- choose(0.0, 100)
+                  return $ Num nbr
+    varaible = return Var
+           | otherwise = oneOf (number, bop, uop, variable)
+             where 
+              number
+  
+rNumber :: Int -> Int ->Gen Num
+rNumber min max = do
+                  nbr <- choose (min, max)
+                  return Num nbr
 
 op :: Gen Op
 op = elements [Mul, Add]
