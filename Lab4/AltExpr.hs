8b4b877292f133107cc061d70be36c36b61ad0ed
Jesper Olsson <jeppealle@hotmail.com>
2015-12-15 10:04:38 +0100

    Some comments and name changing

--- Lab4/AltExpr.hs
+++ Lab4/AltExpr.hs
@@ -6,15 +6,19 @@ import Data.Char
 import Data.Maybe
 import Control.Monad
 
+-- Data type expression witch constructors for Number, Binary Operator
+-- Unary operator and Variable
 data Expr = Num Double
           | Bop Op Expr Expr
           | Uop Fun Expr
           | Var
   deriving Eq
 
+-- Data type for operator with constructors for Addition and Multiplication
 data Op = Add | Mul 
   deriving Eq
 
+-- Data type for function with constructors for sine and cosine
 data Fun = Sin | Cos
   deriving Eq
 
@@ -22,7 +26,7 @@ data Fun = Sin | Cos
 instance Show Expr where
   show = showExpr
 
---Converts an Expression into a String
+-- Function for converting an expression into a nicer looking string
 showExpr :: Expr -> String
 showExpr (Num n) = show n
 showExpr (Bop Add a b) = showExpr a ++ "+" ++ showExpr b
@@ -31,15 +35,19 @@ showExpr (Var) = "x"
 showExpr (Uop Sin z) = "sin " ++ showRads z
 showExpr (Uop Cos z) = "cos " ++ showRads z
 
+-- Helper function for showing a factor of a multiplication with correct 
+-- paranthesis
 showFactor :: Expr -> String
 showFactor (Bop Add a b) = "(" ++ showExpr (Bop Add a b) ++ ")"
 showFactor e = showExpr e
 
+-- Helper function for showing the expressions inside sine and cosine in a correct way
 showRads :: Expr -> String
 showRads (Bop Add a b) = "(" ++ showExpr (Bop Add a b) ++ ")"
 showRads (Bop Mul a b) = "(" ++ showExpr (Bop Mul a b) ++ ")"
 showRads e = showExpr e
 
+-- Function for evaluating a given expression given the value of x
 eval :: Expr -> Double -> Double
 eval (Num n) _ = n
 eval (Bop Add e1 e2) x = (eval e1 x) + (eval e2 x)
@@ -48,59 +56,77 @@ eval (Uop Sin e1) x    = sin (eval e1 x)
 eval (Uop Cos e1) x    = cos (eval e1 x)
 eval (Var) x           = x
 
+-- Function for reading and parsing a given string, resulting in a Maybe expression
 readExpr :: String -> Maybe Expr
 readExpr s = do
-         e <- parse parseAdd (removeSpaces s)
+         e <- parse parseExpr (removeSpaces s)
          return $ fst e
 
-parseAdd :: Parser Expr
-parseAdd = foldr1(Bop Add) `fmap` chain parseMul (parseOperator '+') 
+-- Function that parses adding expressions
+parseExpr :: Parser Expr
+parseExpr = foldr1(Bop Add) `fmap` chain parseTerm (parseOperator '+') 
 
-parseMul :: Parser Expr
-parseMul = foldr1 (Bop Mul) `fmap` chain parseFactor (parseOperator '*')
+-- Function that parses multiplication expressions
+parseTerm :: Parser Expr
+parseTerm = foldr1 (Bop Mul) `fmap` chain parseFactor (parseOperator '*')
 
+-- Function that parses operators
 parseOperator :: Char -> Parser String
 parseOperator c = do
-              string[c]
+                  string[c]
 
+-- Function that parses a factor with possible 
+-- components of it
 parseFactor :: Parser Expr
 parseFactor = parseParenthesis +++
-         parseFunction Sin "sin" +++
-         parseFunction Cos "cos" +++
-         parseVariable +++
-         parseNumber
+              parseFunction Sin "sin" +++
+              parseFunction Cos "cos" +++
+              parseVariable +++
+              parseNumber
 
+-- Function that parses expressions covered in paranthesis
 parseParenthesis :: Parser Expr
 parseParenthesis = do
                    char '('
-                   e <- parseAdd
+                   e <- parseExpr
                    char ')'
                    return $ e
+-- Function that parses a function and returns in with
+-- the unary operator constructor
 parseFunction :: Fun -> String -> Parser Expr
 parseFunction f s = do
                     string s
                     e <- parseFactor
                     return $ Uop f e
 
+-- Function that parses a variable and returns it
+-- with the Var 
 parseVariable :: Parser Expr
 parseVariable = do
                 char 'x'
                 return Var
 
+-- Function that parses a number and returns it
+-- with the Num constructor
 parseNumber :: Parser Expr
 parseNumber = do
               d <- readsP :: Parser Double
               return (Num d)
 
+-- Function that given a string, returns a parser for that string
 string :: String -> Parser String
 string s = sequence $ fmap (char) s
 
+-- Helper function that given a string, removes all it's spaces
+-- and returns the resulting string
 removeSpaces :: String -> String
 removeSpaces [] = ""
 removeSpaces (' ':xs) = removeSpaces xs
 removeSpaces (x:[]) = [x]
 removeSpaces (x:xs) = x : removeSpaces xs
 
+-- Function that simplifies any given expression to the maximum extent.
+-- At the moment, it does not simplify an expression containing a variable
 simplify :: Expr -> Expr
 simplify (Bop o e1 e2) = simplify' (Bop o (simplify e1) (simplify e2))
 simplify (Uop f e) = simplify' (Uop f (simplify e))
@@ -126,7 +152,8 @@ simplify' exp@(Bop Mul e (Num n))
 
 simplify' e = e
 
-
+-- Function that calculates the derivative of any given expression
+-- and returns the resulting expression
 differentiate :: Expr -> Expr
 differentiate (Num _) = Num 0.0
 differentiate (Var)   = Num 1.0
--- Lab4/ExprQC.hs
+++ Lab4/ExprQC.hs
@@ -2,24 +2,55 @@ import Test.QuickCheck
 import Data.Maybe
 import AltExpr
 
+-- Property to check that showing and reading expression does not 
+-- change the value of the initial expression
 prop_ShowReadExpr :: Expr -> Bool
 prop_ShowReadExpr e = closeToEqual e (fromJust(readExpr (showExpr e)))
-  where closeToEqual e1 e2 = abs ((eval' e1) - (eval' e2)) < 0.0001
-        eval' e = eval e 1.37
 
+-- Property to check that simplify does not change the value
+-- of the initial expression 
+prop_simplify :: Expr -> Bool
+prop_simplify e = closeToEqual e (simplify e)
+
+-- Helper function that checks that two expressions are close
+-- to being equal since there seem to be a rounding problem 
+-- with big ńumbers
+closeToEqual :: Expr -> Expr -> Bool
+closeToEqual e1 e2 = abs ((eval' e1) - (eval' e2)) < 0.0001
+  where eval' e = eval e 1.37
+
+-- Function that generates a random expression
 arbExpr :: Int -> Gen Expr
-arbExpr n | n == 1 = oneOf [number, variable]
-  
-rNumber :: Int -> Int ->Gen Num
+arbExpr n | n == 1 = oneof [(rNumber 0 100), variable]
+          | otherwise = oneof [(rNumber 0 100), variable, bop, uop]
+  where
+    bop = do
+         o <- op
+         e1 <- arbExpr(n-1)
+         e2 <- arbExpr (n-1)
+         return (Bop o e1 e2)
+    uop = do
+          f <- fun
+          e <- arbExpr (n-1)
+          return (Uop f e)
+
+-- Helper function that generates a random Num given a min
+-- and a max value  
+rNumber :: Double -> Double -> Gen Expr
 rNumber min max = do
                   nbr <- choose (min, max)
-                  return Num nbr
+                  return (Num nbr)
 
+-- Function that generates a random Variable
+variable :: Gen Expr
+variable = do
+           return Var
 
-
+-- Function that generates a random Binary operator
 op :: Gen Op
 op = elements [Mul, Add]
 
+-- Function that generates a random Unary operator
 fun :: Gen Fun
 fun = elements [Sin, Cos]
 
